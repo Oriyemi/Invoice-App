6b84f1a46efdba510293bd16ada05b043a2d9316
Faith Esan <oriyemiesan@gmail.com>
2026-05-18 17:46:21 +0100

    Enhance invoice creation page with payment status selection and improve layout responsiveness

--- home-page.js
+++ home-page.js
@@ -22,6 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const invoiceDisplay = document.querySelector("#invoice-display");
 
     let allInvoices = [];
+    let status;
 
  
     if (openBtn && modal) {
@@ -40,43 +41,27 @@ document.addEventListener("DOMContentLoaded", () => {
     function createItemRow() {
         const item = document.createElement("div");
 
-        item.className =
-            "grid grid-cols-12 gap-4 items-center mb-4 item-row";
+        item.className = "grid grid-cols-12 gap-4 items-center mb-4 item-row";
 
-        item.innerHTML = `
-            <div class="col-span-5">
-                <input 
-                    type="text"
-                    placeholder="Item Name"
-                    class="w-full border border-[#7E88C3] p-2 rounded dynamic-bg dynamic-sub-text item-name"
-                    required
-                >
+        item.innerHTML = ` <div class="col-span-5">
+                <input   type="text" placeholder="Item Name" class="w-full border border-[#7E88C3] p-2 rounded dynamic-bg dynamic-sub-text item-name" required >
             </div>
 
             <div class="col-span-2">
-                <input 
-                    type="text"
-                    class="w-full border border-[#7E88C3] p-2 rounded dynamic-bg dynamic-sub-text item-qty" required
-                >
+                <input  type="text" class="w-full border border-[#7E88C3] p-2 rounded dynamic-bg dynamic-sub-text item-qty" required >
             </div>
 
             <div class="col-span-2">
-                <input 
-                    type="text"
-                    class="w-full border border-[#7E88C3] p-2 rounded dynamic-bg dynamic-sub-text item-price" required
-                >
+                <input type="text"class="w-full border border-[#7E88C3] p-2 rounded dynamic-bg dynamic-sub-text item-price" required >
             </div>
 
-            <div class="col-span-2 font-bold dynamic-text item-total">
-                0.00
-            </div>
+            <div class="col-span-2 font-bold dynamic-text item-total"> 0.00 </div>
 
             <div class="col-span-1 text-center">
                 <button type="button" class="delete-item text-gray-400 hover:text-red-500">
-                    🗑️
+                 🗑️
                 </button>
-            </div>
-        `;
+            </div> `;
 
         itemList.appendChild(item);
 
@@ -107,10 +92,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
   
     if (emailInput) {
-        emailInput.addEventListener("input", () => {
-            const isValid = emailInput.value.includes("@");
-
-            if (isValid) {
+        emailInput.addEventListener("onchange", () => {
+            const isValid = emailInput.value.includes("@") && emailInput.value.includes(".");
+        
+            if (isValid ) {
                 emailError.textContent = "";
                 emailInput.classList.remove("border-red-500");
             } else {
@@ -126,11 +111,11 @@ document.addEventListener("DOMContentLoaded", () => {
             e.preventDefault();
  
           
-            if (!emailInput.value.includes("@")) {
-                emailError.textContent = "Please enter a valid email";
-                emailInput.classList.add("border-red-500");
-                return;
-            }
+            // if (!emailInput.value.includes("@")) {
+            //     emailError.textContent = "Please enter a valid email";
+            //     emailInput.classList.add("border-red-500");
+            //     return;
+            // }
 
             const formData = new FormData(invoiceForm);
 
@@ -155,13 +140,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
                 paymentTerms:
                     document.getElementById("paymentTerms").value,
+                paymentStatus: document.querySelector("#paymentStatus").value,
+                
 
                 projectDescription:
                     formData.get("projectDescription"),
 
                 items: []
             };
-
+            
            
             document.querySelectorAll(".item-row").forEach((row) => {
                 invoice.items.push({
@@ -174,20 +161,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
           
             allInvoices.push(invoice);
-
-            console.log("Saved Invoice:", invoice);
-
-          
+            status = invoice;
+        
             renderInvoices();
-
-            
+ 
             invoiceForm.reset();
 
-           
             itemList.innerHTML = "";
             createItemRow();
 
-         
             modal.classList.add("hidden");
 
            
@@ -219,44 +201,51 @@ document.addEventListener("DOMContentLoaded", () => {
 
             const card = document.createElement("div");
 
-            card.className =
-                "dynamic-text dynamic-sub-text dynamic-bg  p-6 rounded-lg shadow-sm flex items-center sm:w-[672px] lg:w-[730px] h-[72px] justify-between border border-transparent hover:border-[#7C5DFA] mb-4 font-['League_Spartan'] font-medium text-[13px] leading-[15px] tracking-[-0.1px]";
+             card.className = "dynamic-text dynamic-sub-text dynamic-bg  p-6 rounded-lg shadow-sm flex items-center sm:w-[672px] lg:w-[730px] h-[72px] justify-between border border-transparent hover:border-[#7C5DFA] mb-4 font-['League_Spartan'] font-medium text-[13px] leading-[15px] tracking-[-0.1px]";
 
-            card.innerHTML = `
-                <div class="flex items-center px-6 gap-6">
-                    <span class="font-bold dynamic-text">
-                        #INV-${index + 1}
-                    </span>
+              card.innerHTML = ` <div class="flex items-center px-6 gap-6">
+              <span class="font-bold dynamic-text">
+                #INV-${index + 1}
+              </span>
 
-                    <span class="dynamic-sub-text text-[13px]">
-                        Due ${inv.invoiceDate}
-                    </span>
+               <span class="dynamic-sub-text text-[13px]">
+                 Due ${inv.invoiceDate}
+                </span>
 
-                    <span class="dynamic-sub-text text-[13px]">
-                        ${inv.billTo.clientName}
-                    </span>
-                </div>
+              <span class="dynamic-sub-text text-[13px]">
+                 ${inv.billTo.clientName}
+               </span>  </div>
 
-                <div class="flex items-center gap-8">
-                    <span class="dynamic-text font-bold text-[16px]">
-                        £ ${totalAmount}
-                    </span>
+             <div class="flex items-center gap-8">
 
+                <span class="dynamic-text font-bold text-[16px]">
+                  £ ${totalAmount}
+               </span>
+                 ${inv.paymentStatus === "paid" ? `
                     <div class="bg-[#33d69f14] text-[#33D69F] px-6 py-3 rounded-md flex items-center gap-2">
                         <span class="w-2 h-2 rounded-full bg-[#33D69F]"></span>
-                        <span class="font-bold text-[12px]">Paid</span>
-                    </div>
-                </div>
-            `;
+                        <span class="font-bold text-[12px]">  Paid </span>
+                    </div> ` : inv.paymentStatus === "draft" ? `
+                     <div class="bg-[#f8172a14] text-[#f8172a] px-6 py-3 rounded-md flex items-center gap-2"> <span class="w-2 h-2 rounded-full bg-[#f8172a]"></span> <span class="font-bold text-[12px]"> Draft </span>
+                    </div> `  : inv.paymentStatus === "pending"   ? `<div class="bg-[#7733d614] text-[#7733d6] px-6 py-3 rounded-md flex items-center gap-2">
+                     <span class="w-2 h-2 rounded-full bg-[#7733d6]"></span><span class="font-bold text-[12px]">   Pending  </span>
+                    </div> `: ""
+                }
+                
+                <div class="view-btn cursor-pointer" data-index="${index}">
+                <img src="./src/images/Path 5.png" alt="drop-down">
+                </div> </div> `;
+                
+                invoiceDisplay.appendChild(card);
+            });
 
-            invoiceDisplay.appendChild(card);
-        });
     }
 
 
   
     const dateInput = document.getElementById("invoiceDate");
     const termsSelect = document.getElementById("paymentTerms");
+    const statusSelected=document.querySelector("#paymentStatus")
 
     if (dateInput) {
         const today = new Date().toISOString().split("T")[0];
@@ -272,6 +261,12 @@ document.addEventListener("DOMContentLoaded", () => {
     if (termsSelect) {
         termsSelect.addEventListener("change", (e) => {
             console.log("Selected Term:", e.target.value);
+        });
+    }
+
+    if (statusSelected) {
+        statusSelected.addEventListener("change", (e) => {
+            console.log("Selected Status:", e.target.value);
         });
     }
 });
